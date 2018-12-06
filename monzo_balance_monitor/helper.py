8bf1222d389255f13de27f560093c9bbcbb57a28
Sey Kuyinu <hello@seykuyinu.com>
2018-12-06 23:22:07 +0000

    Get pot id helper function

---
+++ monzo_balance_monitor/helper.py
@@ -0,0 +1,12 @@
+# Helper functions to process Monzo data objects
+
+def get_spending_pot_id(pots_dict, pot_name):
+    pots_list = pots_dict.get('pots')
+
+    spending_pot = list(filter(lambda pot: pot['name'].strip() == pot_name, pots_list))
+    if len(spending_pot) != 1:
+        # TODO: error? or just log?
+        return None
+    else:
+        return spending_pot[0]['id']
+
